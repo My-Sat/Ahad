62ae34286306898a8b465a0a7bfb25147408e36c
My-Sat <chentibusiness@gmail.com>
2025-10-11 10:14:50 +0000

    My third commit

--- app.js
+++ app.js
@@ -1,65 +1,39 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Routers
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
-/**
- * Middleware setup
- */
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Allow CORS (for API or frontend calls)
-app.use(cors({
-  origin: '*', // You can restrict this to your frontend domain later
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/**
- * Catch 404 and forward to error handler
- */
-app.use((req, res, next) => {
-  next(createError(404, 'Resource not found'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-/**
- * Global error handler
- */
-app.use((err, req, res, next) => {
-  // Set locals (useful for rendering error pages)
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Log the error for debugging
-  console.error(`[ERROR] ${err.status || 500} - ${err.message}`);
-
-  // For APIs: return JSON
-  if (req.originalUrl.startsWith('/api/')) {
-    return res.status(err.status || 500).json({
-      success: false,
-      message: err.message || 'Internal Server Error'
-    });
-  }
-
-  // For web routes: render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
