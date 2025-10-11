218aa3d6136bcb08928e3754bc203fe43d366bf1
My-Sat <chentibusiness@gmail.com>
2025-10-11 09:00:55 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+/**
+ * Middleware setup
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Allow CORS (for API or frontend calls)
+app.use(cors({
+  origin: '*', // You can restrict this to your frontend domain later
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/**
+ * Catch 404 and forward to error handler
+ */
+app.use((req, res, next) => {
+  next(createError(404, 'Resource not found'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+/**
+ * Global error handler
+ */
+app.use((err, req, res, next) => {
+  // Set locals (useful for rendering error pages)
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Log the error for debugging
+  console.error(`[ERROR] ${err.status || 500} - ${err.message}`);
+
+  // For APIs: return JSON
+  if (req.originalUrl.startsWith('/api/')) {
+    return res.status(err.status || 500).json({
+      success: false,
+      message: err.message || 'Internal Server Error'
+    });
+  }
+
+  // For web routes: render the error page
   res.status(err.status || 500);
   res.render('error');
 });
